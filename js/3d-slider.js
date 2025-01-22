1f9a172d1b1453f88ffb7c5a3b9eb5b16d5597b0
Your Name <you@example.com>
2025-01-22 13:59:05 +0530

    Modern slider images added for 7 service pages out of 8

--- js/3d-slider.js
+++ js/3d-slider.js
@@ -1,21 +1,56 @@
+// var TrendingSlider = new Swiper('.trending-slider', {
+//     effect: 'coverflow',
+//     grabCursor: true,
+//     centeredSlides: true,
+//     loop: true,
+//     slidesPerView: 'auto',
+//     coverflowEffect: {
+//       rotate: 0,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 2.5,
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: '.button-next',
+//       prevEl: '.button-prev',
+//     }
+//   });
+
+
 var TrendingSlider = new Swiper('.trending-slider', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2.5,
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 3,
+  coverflowEffect: {
+    rotate: 40,
+    stretch: 0,
+    depth: 320,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1
     },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    640: {
+      slidesPerView: 2
     },
-    navigation: {
-      nextEl: '.button-next',
-      prevEl: '.button-prev',
+    1024: {
+      slidesPerView: 3
     }
-  });
+  }
+});
